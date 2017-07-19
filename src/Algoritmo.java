76052e6508a3c78959abda54bb32db1f9cd91488
pao <pao_alfonzo@hotmail.com>
2017-07-19 11:14:59 -0500

    Agregado mas lata al archivo

--- src/Algoritmo.java
+++ src/Algoritmo.java
@@ -3,7 +3,25 @@ public class Algoritmo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hollaaaaaaaaa");
+		System.out.println("hollaaaaaaaaa");
+		
+		int i=0;
+			
+		boolean copia=true;
+		do{
+			System.out.println("Tu nota es"+i);
+			if(i>5)
+			{
+				break;
+			}
+			i++;
+		}while(copia);
+	
 	}
-
+	public static void imprimirTesis(){
+		System.out.println("cualkker coss");
+		System.out.println("mas cosaasss");
+		System.out.println("mas mas");
+		
+	}
 }
